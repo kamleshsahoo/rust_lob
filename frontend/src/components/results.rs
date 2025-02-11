811feb2d35d435ceae6d7bbdaa8baab441f7482c
kamleshsahoo <kamlesh.sahoo20@gmail.com>
2025-02-11 19:58:36 +0100

    result view changes

--- frontend/src/components/results.rs
+++ frontend/src/components/results.rs
@@ -1,12 +1,23 @@
 #![allow(non_snake_case)]
 
+use std::collections::{HashMap, VecDeque};
 use dioxus::prelude::*;
 use rust_decimal::Decimal;
 
-use crate::{components::{orderbook::OrderBookTable, plot::LatencyHistogramv2, trades::Trades}, ExecutedOrders};
+use crate::{components::{orderbook::OrderBookTable, plot::{BarPlotCharming, HistPlotCharming}, trades::{Spread, Trades}}, ExecutedOrders};
 
 #[component]
-pub fn ExecutionView(bid_lvls: Signal<Vec<(Decimal, u64, u64, f32)>>, ask_lvls: Signal<Vec<(Decimal, u64, u64, f32)>>, latency: Signal<Vec<u128>>, fulfilled_orders: Signal<Vec<ExecutedOrders>> ) -> Element {
+pub fn ExecutionView(
+  bid_lvls: ReadOnlySignal<Vec<(Decimal, u64, u64, f32)>>,
+  ask_lvls: ReadOnlySignal<Vec<(Decimal, u64, u64, f32)>>,
+  latency: ReadOnlySignal<Vec<i64>>,
+  fulfilled_orders: ReadOnlySignal<VecDeque<ExecutedOrders>>, latency_by_ordertype: ReadOnlySignal<HashMap<String, Vec<f64>>>,
+  best_bid: ReadOnlySignal<Option<Decimal>>,
+  best_ask: ReadOnlySignal<Option<Decimal>>,
+  spread: ReadOnlySignal<Option<Decimal>>,
+ ) -> Element {
+
+
   rsx! {
     div {
       class: "table-container",
@@ -22,21 +33,28 @@ pub fn ExecutionView(bid_lvls: Signal<Vec<(Decimal, u64, u64, f32)>>, ask_lvls: 
         },
         div {
           class: "table-card spread-card",
-          "Spread -- ()"
+          Spread { best_bid: best_bid(), best_ask: best_ask(), spread: spread() }
         }
       }
     },
     div {
       class: "chart-container",
-      div {
-        class: "chart-card wide-chart",
-        //LatencyHistogramv2 { latencies: latency() }
-        h2 { "Order latency Histogram" },
-        LatencyHistogramv2 { latency }
+      if latency.len() > 0 {
+        div {
+          class: "chart-card wide-chart",
+          //LatencyHistogramv2 { latencies: latency() }
+          // h2 { "Order latency Histogram" },
+          // LatencyHistogramv2 { latency }
+          HistPlotCharming { latency }
+        }
       },
-      div {
-        class: "chart-card",
-        "Boxplot CANVAS"
+      if latency_by_ordertype().len() > 0 {
+        div {
+          class: "chart-card",
+          // h2 { "Latency by Ordertype" },
+          // BarPlot { latency_by_ordertype }
+          BarPlotCharming { latency_by_ordertype }
+        }
       },
       div {
         class: "chart-card",
