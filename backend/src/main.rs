0701a1721469cc579d5e2778bbffc4962c68272b
kamleshsahoo <kamlesh.sahoo20@gmail.com>
2025-02-23 20:32:51 +0100

    file upload route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,20 @@
 mod order_generator;
 mod engine;
 
-use std::net::SocketAddr;
-use axum::{extract::{ws::{Message, WebSocket}, ConnectInfo, WebSocketUpgrade}, response::IntoResponse, routing::any, Router};
+use std::{collections::HashMap, net::SocketAddr, sync::Arc, time::{Duration, Instant}};
+use axum::{body::Bytes, extract::{ws::{Message, WebSocket}, ConnectInfo, State, WebSocketUpgrade}, http::{HeaderValue, Method}, response::IntoResponse, routing::{any, post}, Json, Router};
+use futures::lock::Mutex;
 use futures_util::{SinkExt, StreamExt};
-use order_generator::gen::{ServerMessage, Simulator};
+use order_generator::{gen::{ServerMessage, Simulator}, upload::{process_upload_orders, FileUploadOrderType, FinalStats, UploadError, UploadRequest, UploadResponse}};
 use serde::Deserialize;
-use tokio::{net::TcpListener, sync::mpsc::{self, Sender}};
+use tokio::{net::TcpListener, sync::mpsc, time::sleep};
+use tower_http::cors::CorsLayer;
+use uuid::Uuid;
 
+type OrderSender = mpsc::Sender<Vec<FileUploadOrderType>>;
+type OrderReceiver = mpsc::Receiver<Vec<FileUploadOrderType>>;
+type ResultSender = mpsc::Sender<HashMap<String, FinalStats>>;
+type ResultReceiver = mpsc::Receiver<HashMap<String, FinalStats>>;
 
 #[derive(Debug, Deserialize)]
 #[serde(tag = "type")]
@@ -29,47 +36,97 @@ enum Simulation {
   Complete
 }
 
-/*
-struct WebSocketBatcher {
-  last_update: Instant,
-  batch_interval: Duration,
-  engine_stats: Vec<EngineStats>,
-  trades: Vec<ExecutedOrders>
+struct FileUploadSessionData {
+  order_sender: OrderSender,
+  result_receiver: ResultReceiver,
+  created_at: Instant,
+  total_chunks: usize,
+  total_orders: usize,
+  processed_chunks: usize
 }
 
-impl WebSocketBatcher {
-  fn new(socket_batch_interval: Duration) -> Self {
-    Self {
-      last_update: Instant::now(),
-      batch_interval: socket_batch_interval,
-      engine_stats: Vec::new(),
-      trades: Vec::new(),
-    }
-  }
-
-  fn should_send(&self) -> bool {
-    self.last_update.elapsed() >= self.batch_interval
-  }
-
-  fn reset_timer(&mut self) {
-    self.last_update = Instant::now();
-  }
-
+#[derive(Clone)]
+struct UploadSessionManager {
+  sessions: Arc<Mutex<HashMap<String, FileUploadSessionData>>>
 }
-*/
+
+impl UploadSessionManager {
+  fn new() -> Self {
+    Self { sessions: Arc::new(Mutex::new(HashMap::new())) }
+  }
+
+  async fn create_session(&self, session_id: String, total_chunks: usize, total_orders: usize) -> OrderSender {
+    let (order_tx, order_rx) = mpsc::channel(1_000);
+    let (result_tx, result_rx) = mpsc::channel(1_000);
+
+    let session = FileUploadSessionData {
+      order_sender: order_tx.clone(),
+      result_receiver: result_rx,
+      created_at: Instant::now(),
+      total_chunks,
+      total_orders,
+      processed_chunks: 0
+    };
+
+    let mut sessions = self.sessions.lock().await;
+    sessions.insert(session_id, session);
+
+    Self::spawn_processing_task(order_rx, result_tx, total_orders);
+
+    // (order_rx, result_tx)
+    order_tx
+  }
+
+  fn spawn_processing_task(mut order_rx: OrderReceiver, result_tx: ResultSender, total_orders: usize) {
+    tokio::spawn(async move {
+      let mut all_orders = Vec::new();
+        while let Some(chunk) = order_rx.recv().await {
+          all_orders.extend(chunk);
+          // process when all orders are recvd
+          if all_orders.len() >= total_orders {
+            let result = process_upload_orders(all_orders).await;
+            result_tx.send(result).await.expect("failed to send final result on channel!");
+            break;
+          }
+        }
+    });
+  }
+
+  async fn get_chunk_sender(&self, session_id: &str) -> Option<OrderSender> {
+    let sessions = self.sessions.lock().await;
+    sessions.get(session_id).map(|s| s.order_sender.clone())
+  } 
+
+  async fn take_result_receiver(&self, session_id: &str) -> Option<ResultReceiver> {
+    let mut sessions = self.sessions.lock().await;
+    sessions.remove(session_id).map(|s| s.result_receiver)
+  }
+  /* TODO: Cleanup old sessions */
+} 
+
 
 #[tokio::main]
 async fn main() {
+
+  let session_manager = UploadSessionManager::new();
+
+  let cors = CorsLayer::new()
+  .allow_methods([Method::GET, Method::POST])
+  .allow_origin("http://127.0.0.1:8080".parse::<HeaderValue>().unwrap());
+  // .allow_headers([CONTENT_TYPE]);
     
-  let app = Router::new().route("/wslob", any(handler));
+  let app = Router::new()
+  .route("/wslob", any(ws_handler))
+  .route("/file", post(upload_handler))
+  .with_state(session_manager)
+  .layer(cors);
 
   let listener = TcpListener::bind("0.0.0.0:7575").await.expect("failed to start tcp listener");
 
   axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.expect("failed to start server");
-
 }
 
-async fn handler (ws: WebSocketUpgrade, ConnectInfo(addr): ConnectInfo<SocketAddr>) -> impl IntoResponse {
+async fn ws_handler (ws: WebSocketUpgrade, ConnectInfo(addr): ConnectInfo<SocketAddr>) -> impl IntoResponse {
   // TODO: maybe add UserAgent info!
   println!("User at {} connected.", addr);
   ws.on_upgrade(move|socket| handle_socket(socket, addr))
@@ -80,8 +137,11 @@ async fn handle_socket(socket: WebSocket, who: SocketAddr) {
   let (mut sender, mut receiver) = socket.split();
   let (tx, mut rx) = mpsc::channel(1_000_000);
 
-  // let mut ws_batcher = WebSocketBatcher::new(Duration::from_micros(10));
   let mut batch: Vec<Vec<ServerMessage>>  = vec![];
+  let mut batch_count: usize = 0;
+  const BATCH_SIZE: usize = 500;
+  const BREATHE_AFTER_BATCHES: usize = 10;
+  const BREATHING_TIME_MS: u64 = 1;
 
   loop { 
     tokio::select! {
@@ -94,29 +154,18 @@ async fn handle_socket(socket: WebSocket, who: SocketAddr) {
               match payload {
                 ClientMessage::Start {total_objects, throttle_nanos, mean_price, sd_price , best_price_levels} => {
                   println!("client payload\ntotal objects: {:?} nanos: {:?} mean: {:?} sd: {:?} show best price levels: {:?}", total_objects, throttle_nanos, mean_price, sd_price, best_price_levels);
-            
-                  // let (throttle, num_orders) = (throttle_nanos.unwrap_or(1_000_000_000), total_objects.unwrap_or(1_000));
-                  // let (mean, sd, best_price_lvls) = (mean_price.unwrap_or(300.0), sd_price.unwrap_or(50.0), best_price_levels.unwrap_or(false));
-
+                  // spawn a task 
                   tokio::spawn(process_start_message(tx.clone(), total_objects, mean_price, sd_price, best_price_levels, throttle_nanos));
                },
                ClientMessage::Stop => {
                 println!(">>> {} requested STOP", who);
-                // if let Some(handle) = process_handle.take() {
-                //   handle.abort();
-                // }
-                // if tx.send(Message::Close(Some(CloseFrame {
-                //   code: NORMAL,
-                //   reason: Utf8Bytes::from_static("Client Requested STOP")
-                // }))).await.is_err() { 
-                //   println!("Could not send Close frame to channel after client STOP request!");
-                // }
+                
 
                 break;
                }
               }
             },
-            Message::Close(c) => {
+            Message::Close(_c) => {
               println!(">> {} sent CloseFrame msg", who);
               break;
             },
@@ -133,13 +182,19 @@ async fn handle_socket(socket: WebSocket, who: SocketAddr) {
           Simulation::Complete => {
             println!("Simulation complete. Closing the simulation channel and droping the socket connection");
             if !batch.is_empty() {
-              //println!("sending rem updates: {:?}", &batch.len());
+              println!("sending rem updates: {:?}", &batch.len());
               let update_msg = Message::text(serde_json::to_string(&batch).expect("serializing final server updates failed!"));
               if sender.send(update_msg).await.is_err() {
                 break;
               }
             }
             rx.close();
+
+            let completion_signal = ServerMessage::Completed;
+            let completion_msg = Message::text(serde_json::to_string(&vec![[completion_signal]]).expect("serializing simulation completion signal failed!"));
+            if sender.send(completion_msg).await.is_err() {
+              println!("sending simulation completion signal to dioxus failed!");
+            }
             break;
           },
           Simulation::Start(snapshot) => {
@@ -153,92 +208,23 @@ async fn handle_socket(socket: WebSocket, who: SocketAddr) {
             //<< Send batched updates to the Websocket Client >>
             batch.push(data_updates);
 
-            if batch.len() >= 1_000 {
+            if batch.len() >= BATCH_SIZE {
               //println!("batch: {:?}", &batch.len());
+              // let batch_size = mem::size_of_val(&*batch);
+              // println!("size of messages: {:?}", batch_size);
               let update_msg = Message::text(serde_json::to_string(&batch).expect("serializing server updates failed!"));
               if sender.send(update_msg).await.is_err() {
                 break;
               }
               batch.clear();
-            }
-          }
+              batch_count += 1;
 
-
-          /*Working Ver with time based batching
-          Simulation::Data(mut data_updates) => {
-            // << Send batched updates to the Websocket Client >>
-            
-            // extract the engine stats (1st element) and store
-            if let ServerMessage::ExecutionStats(current_stat) = data_updates.remove(0) {
-              ws_batcher.engine_stats.extend(current_stat);
-            }
-
-            // extract new trades (if any) which would be the last elem
-            if let Some(ServerMessage::Trades(t)) = data_updates.last() {
-              //println!("new trades: {:?}", &t);
-              if let Some(ServerMessage::Trades(new_trades)) = data_updates.pop() {
-                ws_batcher.trades.extend(new_trades);
+              if batch_count % BREATHE_AFTER_BATCHES == 0 {
+                sleep(Duration::from_millis(BREATHING_TIME_MS)).await;
               }
-            };
-
-            // check if its time to send update
-            if ws_batcher.should_send() {
-              
-              data_updates.push(ServerMessage::ExecutionStats(ws_batcher.engine_stats.clone()));
-              
-              if !ws_batcher.trades.is_empty() {
-                data_updates.push(ServerMessage::Trades(ws_batcher.trades.clone()))
-              };
-              
-              let update_msg = Message::text(serde_json::to_string(&data_updates).expect("serializing server updates failed!"));
-              if sender.send(update_msg).await.is_err() {
-                break;
-              }
-              // reset the timer & clear the buffer
-              ws_batcher.engine_stats.clear();
-              ws_batcher.trades.clear();
-              ws_batcher.reset_timer();
-
             }
-          },
-          */
+          }
         }
-        
-        /*Working Ver with serialized msg on channel
-        if let Message::Close(c) = msg {
-          if let Some(cf) = c {
-            println!("CloseChannel frame: code {} and reason: {}", cf.code, cf.reason)
-          } else{
-            println!(">>> Somehow got close channel request without CloseFrame");
-          }
-          rx.close();
-          break;
-        } else {
-          // Sending to the Client>>
-          if last_update.elapsed() >= ws_send_freq {
-
-            if sender.send(msg).await.is_err() {
-            break;
-          }
-          last_update = Instant::now();
-        }
-
-          /* Working ver with batches
-          if sender.feed(msg).await.is_err(){
-            println!("sending simulator updates via feed to dioxus server failed!");
-            break;
-          }
-
-          batch_cntr += 1;
-          
-          if batch_cntr >= 100 {
-            sender.flush().await.expect("flushing failed while sending batched updates to dioxus server!");
-            batch_cntr = 0;
-          }
-          */
-        }
-        */
-
       }
     }
   }
@@ -246,9 +232,8 @@ async fn handle_socket(socket: WebSocket, who: SocketAddr) {
   println!("Websocket context {} destroyed", who);
 }
 
-async fn process_start_message(tx: Sender<Simulation>, num_orders: usize, mean_price: f64, sd_price: f64, best_price_lvls: bool, throttle: u64) {
+async fn process_start_message(tx: mpsc::Sender<Simulation>, num_orders: usize, mean_price: f64, sd_price: f64, best_price_lvls: bool, throttle: u64) {
   //TODO: see if we need to add throttling
-  
   let mut simulator = Simulator::new(mean_price, sd_price, best_price_lvls);
   // seed the orderbook with 10k ADD limit orders
   simulator.seed_orderbook(10_000);
@@ -260,7 +245,6 @@ async fn process_start_message(tx: Sender<Simulation>, num_orders: usize, mean_p
 
   println!("[INFO] Starting simulation");
   for idx in 0..num_orders {
-    // interval.tick().await;
     // generate and process the orders
     simulator.generate_orders();
     let updates = simulator.generate_updates(idx);
@@ -268,21 +252,115 @@ async fn process_start_message(tx: Sender<Simulation>, num_orders: usize, mean_p
     if tx.send(Simulation::Data(updates)).await.is_err() {
       panic!("receiver half of channel dropped!");
     };
-    
   }
   //println!("trades: {:?}", simulator.book.executed_orders);
   println!("[INFO] Completed simulation (total trades: {:?}), sending close frame to channel", simulator.book.executed_orders.len());
-  // if tx.send(Message::Close(Some(CloseFrame {
-  //   code: NORMAL,
-  //   reason: Utf8Bytes::from_static("Streaming complete")
-  // }))).await.is_err() { 
-  //   println!("Could not send Close frame to channel after simulation complete!");
-  // }
-
-  // sleep a bit before dropping the connection
-  // sleep(Duration::from_millis(5)).await;
 
   if tx.send(Simulation::Complete).await.is_err() { 
     panic!("Could not send close signal to channel after simulation was complete!");
+  }
+}
+
+async fn upload_handler(State(session_manager): State<UploadSessionManager>, body: Bytes) -> Result<Json<UploadResponse>, UploadError> {
+  
+  //println!("User uploaded file.");
+  let payload = match <UploadRequest>::deserialize(&mut rmp_serde::Deserializer::new(&body[..])) {
+    Ok(de_payload) => de_payload,
+    Err(e) => {
+      println!("couldnot deserialize the client file orders: {:?}", e);
+      return Err(UploadError::DeserializeError(e.to_string()))
+    }
+  };
+
+  let session_id = match payload.session_id {
+    Some(id) => id,
+    None => Uuid::new_v4().to_string()
+  };
+  
+  match payload.chunk_number {
+    0 => {
+      let order_tx = session_manager.create_session(session_id.clone(), payload.total_chunks, payload.total_orders).await;
+      // let total_orders = payload.total_orders;
+      if let Err(e) = order_tx.send(payload.orders).await {
+        println!("sending 1st chunk on channel failed!");
+        return Err(UploadError::ChannelError(e.to_string()));
+      }
+
+      // check if we have only one chunk
+      if payload.total_chunks != 1 {
+        return Ok(Json(
+          UploadResponse {
+            data: None,
+            session_id: Some(session_id),
+          }
+        ));
+      } else {
+        match session_manager.take_result_receiver(&session_id).await {
+          Some(mut rcvr) => {
+            match rcvr.recv().await {
+              Some(res) => {
+                return Ok(Json(
+                  UploadResponse {
+                    data: Some(res),
+                    session_id: Some(session_id),
+                  }
+                ));
+              },
+              None => return Err(UploadError::ChannelError("sender dropped when receiving the final result".to_string()))
+            }
+          },
+          None => return Err(UploadError::SessionNotFound)
+        };
+      }
+    },
+    n if n == payload.total_chunks - 1 => {
+      // last chunk
+      let order_sender = session_manager.get_chunk_sender(&session_id).await;
+      let result_receiver = session_manager.take_result_receiver(&session_id).await;
+      match (order_sender, result_receiver) {
+        (Some(sender), Some(mut rcvr)) => {
+          // send the final chunk
+          if let Err(e) = sender.send(payload.orders).await {
+            println!("the mpsc orders receiver dropped");
+            return Err(UploadError::ChannelError(e.to_string()));
+          }
+
+          match rcvr.recv().await {
+            Some(res) => {
+              return Ok(Json(
+                UploadResponse {
+                  data: Some(res),
+                  session_id: Some(session_id),
+                }
+              ));
+            },
+            None => return Err(UploadError::ChannelError("sender dropped when receiving the final result".to_string()))
+          }
+        },
+        _ => return Err(UploadError::SessionNotFound),
+      } 
+    },
+    n if n < payload.total_chunks - 1 => {
+      // for subsequent chunks
+      match session_manager.get_chunk_sender(&session_id).await {
+          Some(order_sender) => {
+            if let Err(e) = order_sender.send(payload.orders).await {
+              println!("the mpsc orders receiver dropped");
+              return Err(UploadError::ChannelError(e.to_string()));
+            };
+            return Ok(
+              Json(
+                UploadResponse {
+                  data: None,
+                  session_id: Some(session_id),
+                }
+              ));
+          },
+          None => {
+            return Err(UploadError::SessionNotFound);
+          }
+      }  
+    },
+    _ => return Err(UploadError::InvalidChunk)
   }
 }
