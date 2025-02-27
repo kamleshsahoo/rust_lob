b4706d238408175466b3053de2ab7c9bc89061fd
kamleshsahoo <kamlesh.sahoo20@gmail.com>
2025-02-27 12:28:57 +0100

    get cumlative stats

--- frontend/src/utils/enginestats.rs
+++ frontend/src/utils/enginestats.rs
@@ -1,63 +1,32 @@
-// use std::time::Duration;
-
-/* TODO: remove if not needed
-use tokio::time::Duration;
-
-pub struct LatencyProcessor {
-  latency_buffer: Vec<u128>,
-  update_interval: tokio::time::Interval,
-  window_size: usize
-}
-
-impl LatencyProcessor {
-  pub fn new() -> Self {
-    LatencyProcessor {
-      latency_buffer: Vec::with_capacity(1_000), 
-      update_interval: tokio::time::interval(Duration::from_millis(15)),
-      window_size: 1_000
-    }
-  }
-
-  pub fn process_latency(&mut self, latency: u128) {
-    self.latency_buffer.push(latency)
-  }
-
-  pub fn get_latency_update(&mut self) -> Option<Vec<u128>> {
-    if self.latency_buffer.is_empty() {
-      return None;
-    }
-    let current_latency_buffer_len = self.latency_buffer.len();
-    if current_latency_buffer_len > self.window_size {
-      self.latency_buffer.drain(..current_latency_buffer_len - self.window_size);
-    }
-    Some(self.latency_buffer.clone())
-  }
-
-  pub async fn should_update(&mut self) -> bool {
-    let _instant = self.update_interval.tick().await;
-    true
-  }
-}
-*/
+use std::collections::{BTreeMap, HashMap};
+use charming::datatype::{CompositeValue, DataPoint, NumericValue};
+use dioxus::logger::tracing::info;
+//use dioxus::logger::tracing::info;
+use crate::pages::simulator::{EngineStats, ExecutedOrders};
 
 /* Per order we get 
 {order_type:"ADD"/"MODIFY"/"CANCEL", latency, avl_rebalances, executed_orders_cnt}
 */
 
-use std::collections::HashMap;
-use charming::datatype::{CompositeValue, DataPoint, NumericValue};
-use crate::EngineStats;
+fn mean(vec: &Vec<i64>) -> f64 {
+  let n = vec.len();
+  let mut sum = 0;
+  
+  for i in vec { 
+    sum += *i;
+  }
+  (sum as f64)/ (n as f64)
+}
 
-fn quantiles(vec: &mut Vec<i64>, qvals: &Vec<f64>) -> Vec<f64> {
+
+fn quantiles_with_mean(vec: &mut Vec<i64>, qvals: &Vec<f64>) -> Vec<f64> {
   /*
-  calculates a non-parametric estimate if the inv cumulative distributive function
-  uses `linear` interpolation
+  calculates a non-parametric estimate of the inv cumulative distributive function (cdf) using linear interpolation
   `(1-g)*y[j] + g*y[j+1]``
   where the index j and coefficient g are the integral and fractional components
   of q * (n-1), and n is the number of elements in the sample.
   NOTE: result includes average as the last element
   */
-
   let n = vec.len();
   vec.sort();
   let mut result: Vec<f64> = vec![];
@@ -74,18 +43,14 @@ fn quantiles(vec: &mut Vec<i64>, qvals: &Vec<f64>) -> Vec<f64> {
   }
   
   // last element is average/mean
-  let mut sum = 0;
-  for i in vec {
-      sum += *i;
-  }
-  result.push((sum as f64)/ (n as f64));
+  result.push(mean(vec));
   
   result
 }
 
-pub fn get_latency_by_ordertype<'a>(stats: &'a Vec<EngineStats>, qvals: &'a Vec<f64>) -> HashMap<String, Vec<f64>> {
-  
-  let mut latency_fold = stats.iter().fold(HashMap::new(), |mut map_state, e| {
+pub fn get_latency_by_ordertype<'a>(stats: &'a Vec<EngineStats>, qvals: &'a Vec<f64>) ->  HashMap<String, Vec<f64>> {
+
+  let mut ordertype_fold = stats.iter().fold(HashMap::new(), |mut map_state, e| {
     match e.order_type.as_str() {
       "ADD" => { 
         map_state.entry("ADD").or_insert(Vec::<i64>::new()).push(e.latency)
@@ -96,18 +61,38 @@ pub fn get_latency_by_ordertype<'a>(stats: &'a Vec<EngineStats>, qvals: &'a Vec<
       "CANCEL" => {
         map_state.entry("CANCEL").or_insert(Vec::<i64>::new()).push(e.latency)
       },
-      _ => panic!("unsupported order type in engine stat!")
+      _ => unreachable!("unsupported order type in engine stat!")
     }
     map_state
   });
 
-  let latency_fold_stats = latency_fold.iter_mut().map(|(k, v)| { 
-    let stats = quantiles(v, qvals);
+  let ordertype_fold_stats: HashMap<String, Vec<f64>> = ordertype_fold.iter_mut().map(|(k, v)| { 
+    let stats = quantiles_with_mean( v, qvals);
     (String::from(*k), stats)
   }).collect::<HashMap<_,_>>();
 
-  latency_fold_stats
+  ordertype_fold_stats
 
+}
+
+pub fn get_latency_by_avl_trades<'a>(stats: &'a Vec<EngineStats>) -> BTreeMap<(i64, i64), f64> {
+
+  let trades_avl_fold = stats.iter().fold(
+    BTreeMap::new(), |mut map_state, e| { 
+      map_state
+      .entry((e.executed_orders_cnt as i64, e.avl_rebalances))
+      .or_insert(Vec::<i64>::new())
+      .push(e.latency);
+      map_state
+    }
+  );
+  // may use filter_map
+  let trades_avl_fold_stats:BTreeMap<(i64, i64), f64>  = trades_avl_fold.iter().map(|(k, v)| {
+    let avg_latency = mean(v); 
+    (*k, avg_latency)
+  }).collect::<BTreeMap<_,_>>();
+  
+  trades_avl_fold_stats
 }
 
 #[derive(Debug)]
@@ -156,10 +141,8 @@ pub fn bin_data(data: &Vec<i64>, bin_width: i64, max_value: i64) ->  Vec<DataPoi
     BinRange::Fixed(lower, _) | BinRange::CatchAll(lower) => lower
   });
  
-
   // let _u: Vec<_> = result.drain(num_bins as usize..).collect();
   // info!("excluded bin: {:?}", _u);
-  
   let processed_bins = result.into_iter().filter_map(|bin| {
     match bin.range {
       BinRange::Fixed(x0, x1) => {
@@ -172,7 +155,7 @@ pub fn bin_data(data: &Vec<i64>, bin_width: i64, max_value: i64) ->  Vec<DataPoi
           CompositeValue::Number(NumericValue::Integer(x0)),
           CompositeValue::Number(NumericValue::Integer(x1)),
           CompositeValue::String(label)
-        ])))      
+        ])))        
         },
         BinRange::CatchAll(_) => None
       }
@@ -180,4 +163,28 @@ pub fn bin_data(data: &Vec<i64>, bin_width: i64, max_value: i64) ->  Vec<DataPoi
   ).collect::<Vec<_>>();
 
   processed_bins
+}
+
+pub fn bar3d_data(data: &BTreeMap<(i64, i64), f64>) -> Vec<Vec<CompositeValue>> {
+  let mut data_3d: Vec<Vec<CompositeValue>> = vec![];
+  // data_3d.push(vec![CompositeValue::String(String::from("Trades")), CompositeValue::String(String::from("AVL")), CompositeValue::String(String::from("Latency"))]);
+
+  for (k, v) in data.iter() {
+    let trade_cnt = CompositeValue::Number(NumericValue::Integer(k.0));
+    let avl_cnt = CompositeValue::Number(NumericValue::Integer(k.1));
+    let avg_latency = CompositeValue::Number(NumericValue::Integer(*v as i64));
+    data_3d.push(vec![trade_cnt, avl_cnt, avg_latency]);
+  }
+  data_3d
+}
+
+pub fn get_cumlative_results(engine_stats: Vec<EngineStats>, executed_orders: Vec<ExecutedOrders>, qvals: Vec<f64>) -> (Vec<i64>, HashMap<String, Vec<f64>>, BTreeMap<(i64, i64), f64>) {
+  info!("Simulation complete. Computing cumlative results");
+  info!("engine stats vec len: {:?} trades: {:?}", engine_stats.len(), executed_orders.len());
+
+  let latencies = engine_stats.iter().map(|e| e.latency).collect::<Vec<i64>>();
+  let latencies_by_ordertype = get_latency_by_ordertype(&engine_stats, &qvals);
+  let latencies_by_avl_trades = get_latency_by_avl_trades(&engine_stats);
+
+  (latencies, latencies_by_ordertype, latencies_by_avl_trades)
 }
