e97136cb83dc2c6093a8d9952d0b12de497da982
kamleshsahoo <kamlesh.sahoo20@gmail.com>
2025-02-28 15:18:34 +0100

    update mods

--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -1,3 +1,8 @@
 pub mod formDialog;
-pub mod priceLvls;
-pub mod plot;
+pub mod orderbook;
+pub mod trades;
+pub mod plot_rt;
+pub mod plot_st; 
+pub mod modeselect;
+pub mod results;
+pub mod template;
