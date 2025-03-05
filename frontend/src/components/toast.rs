ffdecf929c306a50cd38f9c8fb798dc5b785f42e
kamleshsahoo <kamlesh.sahoo20@gmail.com>
2025-03-05 23:46:35 +0100

    toasts

---
+++ frontend/src/components/toast.rs
@@ -0,0 +1,57 @@
+use dioxus::prelude::*;
+
+#[component]
+pub fn SuccessToast(id: &'static str, content: &'static str) -> Element {
+  rsx! {
+    div {
+      id: id,
+      class: "toast success",
+      div {
+        class: "toast-icon",
+        svg {
+          xmlns: "http://www.w3.org/2000/svg",
+          width: "20",
+          height: "20",
+          view_box: "0 0 20 20",
+          fill: "none",
+          path {
+            d: "M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z",
+            fill: "#10b981"
+          }
+        }
+      }
+      div {
+        class: "toast-content",
+        {content}
+      }
+    }
+  }
+}
+
+#[component]
+pub fn ErrorToast(id: &'static str, content: &'static str) -> Element {
+  rsx! {
+    div {
+      id: id,
+      class: "toast error",
+      div {
+        class: "toast-icon",
+        svg {
+          xmlns: "http://www.w3.org/2000/svg",
+          width: "20",
+          height: "20",
+          view_box: "0 0 20 20",
+          fill: "none",
+          path {
+            d: "M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM11 15H9V13H11V15ZM11 11H9V5H11V11Z",
+            fill: "#ef4444"
+          }
+        }
+      }
+      div {
+        class: "toast-content",
+        {content}
+      }
+    }
+  }
+}
