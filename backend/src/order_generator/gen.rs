5fb3e1bdc00efa5cc04f4c81999d0da3d54bfa61
kamleshsahoo <kamlesh.sahoo20@gmail.com>
2025-03-15 22:54:00 +0100

    take order probs as inputs

--- backend/src/order_generator/gen.rs
+++ backend/src/order_generator/gen.rs
@@ -1,5 +1,4 @@
 use std::{str::FromStr, time::Instant};
-// use chrono::{DateTime, Utc};
 use rand::{rngs::StdRng, SeedableRng};
 use rand_distr::{Bernoulli, Distribution, Normal, Uniform};
 use rust_decimal::{prelude::ToPrimitive, Decimal};
@@ -9,12 +8,13 @@ use crate::engine::orderbook::{Arena, BidOrAsk, ExecutedOrders};
 
 #[derive(Debug, Serialize)]
 // #[serde(tag = "type")]
-pub enum ServerMessage {
+pub enum WsResponse {
   PriceLevels { snapshot: bool, bids: Vec<(Decimal, u64)>, asks: Vec<(Decimal, u64)> },
   Trades (Vec<ExecutedOrders>),
   ExecutionStats (EngineStats),
   BestLevels {best_buy: Option<Decimal>, best_sell: Option<Decimal>},
-  Completed
+  Completed,
+  RateLimitExceeded
 }
 
 #[derive(Debug, Serialize, Clone)]
@@ -26,8 +26,6 @@ pub struct EngineStats {
 }
 
 pub struct Simulator {
-  // symbol: String,
-  // sequence_number: u64,
   pub book : Arena,
   engine_stats: Vec<EngineStats>,
   rng: StdRng,
@@ -43,10 +41,8 @@ pub struct Simulator {
 }
 
 impl Simulator {
-  pub fn new(mean_price: f64, sd_price: f64, best_price_lvls: bool) -> Self {
-    //TODO: could take indvidual these probs as input
-    let action_probs = vec![0.0, 0.4, 0.6]; // ADD, CANCEL, MODIFY
-
+  pub fn new(mean_price: f64, sd_price: f64, order_probs: Vec<f32>, best_price_lvls: bool) -> Self {
+    //let order_probs = vec![0.0, 0.4, 0.6]; // ADD, CANCEL, MODIFY
     Simulator {
       book: Arena::new(best_price_lvls),
       engine_stats: Vec::new(),
@@ -55,7 +51,7 @@ impl Simulator {
       mean_limit_price: mean_price,
       sd_limit_price: sd_price,
       order_type_dist: Uniform::new(0.0, 1.0).expect("error creating uniform dist for order type"),
-      order_type_cuml_probs: action_probs.into_iter().scan(0.0, |acc, x| { 
+      order_type_cuml_probs: order_probs.into_iter().scan(0.0, |acc, x| { 
         *acc += x;
         Some(*acc)
       }).collect(),
@@ -67,7 +63,7 @@ impl Simulator {
   }
 
   fn create_add_limit(&mut self) {
-    //println!("**ADD");
+    // println!("**ADD");
     let shares = self.qty_dist.sample(&mut self.rng);
     let side = self.side_dist.sample(&mut self.rng);
 
@@ -104,7 +100,7 @@ impl Simulator {
   }
 
   fn create_cancel_limit(&mut self) {
-    match self.book.get_random_order_id() {
+    match self.book.get_random_order_id(&mut self.rng) {
       None => self.create_add_limit(),
       Some(order_id) => {
         let start = Instant::now();
@@ -120,7 +116,7 @@ impl Simulator {
     let highest_buy = self.book.highest_buy.unwrap().to_f64().unwrap();
     let price_distr = Normal::new(highest_buy, self.sd_limit_price).expect("error creating a normal dist for modify limit!");
 
-    match self.book.get_random_order_id() {
+    match self.book.get_random_order_id(&mut self.rng) {
       None => self.create_add_limit(),
       Some(order_id) => {
         let order = self.book.orders.get(order_id).expect("order should exist after the checks!");
@@ -176,11 +172,11 @@ impl Simulator {
 
     match self.order_type_cuml_probs.iter().position(|cumprob| rand_num <= *cumprob).expect("error getting order type idx!") {
       0 => {
-        //println!("inserting ADD with order id: {:?}", self.order_id);
+        //println!("ADD trigg with order id: {:?}", self.order_id);
         self.create_add_limit()
       },
       1 => {
-        //println!("CANCEL trigg. current order id: {:?}", self.order_id);
+        // println!("CANCEL trigg. current order id: {:?}", self.order_id);
         self.create_cancel_limit()},
       2 => {
         //println!("MODIFY trigg. curr order id: {:?}", self.order_id);
@@ -190,30 +186,30 @@ impl Simulator {
     };
   } 
 
-  pub fn get_snapshot(&self) -> Vec<ServerMessage> {
-    vec![ServerMessage::PriceLevels { snapshot: true, bids: self.book.get_top_n_bids(20), asks: (self.book.get_top_n_asks(20)) }]
+  pub fn get_snapshot(&self) -> Vec<WsResponse> {
+    vec![WsResponse::PriceLevels { snapshot: true, bids: self.book.get_top_n_bids(20), asks: (self.book.get_top_n_asks(20)) }]
   }
   
-  pub fn generate_updates(&mut self, idx: usize) -> Vec<ServerMessage>{
+  pub fn generate_updates(&mut self, idx: usize) -> Vec<WsResponse>{
     
     let mut messages = Vec::new();
     // always send the engine stats
     let engine_stat = self.engine_stats.get(idx).expect("each order should have a execution stat!").clone();
-    messages.push(ServerMessage::ExecutionStats(engine_stat));
+    messages.push(WsResponse::ExecutionStats(engine_stat));
     
     // sending top `n=1000` price levels 
     // NOTE: bids or asks may be empty vectors
     if (idx+1) % 100 == 0 {
-      let price_levels = ServerMessage::PriceLevels { snapshot: false, bids: self.book.get_top_n_bids(1_000), asks: (self.book.get_top_n_asks(1_000)) }; 
+      let price_levels = WsResponse::PriceLevels { snapshot: false, bids: self.book.get_top_n_bids(1_000), asks: (self.book.get_top_n_asks(1_000)) }; 
       messages.push(price_levels);
     }
 
     if idx % 100 == 0 {
-      messages.push(ServerMessage::BestLevels { best_buy: self.book.highest_buy, best_sell: self.book.lowest_sell });
+      messages.push(WsResponse::BestLevels { best_buy: self.book.highest_buy, best_sell: self.book.lowest_sell });
     }
 
     if let Some(trades) = self.book.get_executed_orders(&mut self.executed_orders_offset) {
-      messages.push(ServerMessage::Trades(trades));
+      messages.push(WsResponse::Trades(trades));
     }  
     messages
   }
