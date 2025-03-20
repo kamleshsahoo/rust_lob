b2622000fe05ae2970f7c173624dad3b28a8a22c
kamleshsahoo <kamlesh.sahoo20@gmail.com>
2025-03-20 11:06:59 +0100

    live feed fixes

--- frontend/src/utils/priceupdate.rs
+++ frontend/src/utils/priceupdate.rs
@@ -19,7 +19,6 @@ impl PriceLevelProcessor {
   }
 
   fn apply_deltas(&self, current_levels: Vec<(Decimal, u64)>, orders: Vec<(Decimal, u64)>, isAsk: bool) -> Vec<(Decimal, u64)> {
-    //let mut updated_levels = current_levels.clone();
     let order_map: HashMap<Decimal, u64> = orders.into_iter().collect();
     // Keep only existing levels that are still in orders and update their volumes
     let mut updated_levels: Vec<(Decimal, u64)> = current_levels
@@ -86,49 +85,37 @@ impl PriceLevelProcessor {
   }
 
   pub fn updater(&mut self, snapshot: bool, bids: Vec<(Decimal, u64)>, asks: Vec<(Decimal, u64)>, mut raw_bids: Signal<Vec<(Decimal, u64)>>, mut raw_asks: Signal<Vec<(Decimal, u64)>>, mut bid_lvls: Signal<Vec<(Decimal, u64, u64, f32)>>, mut ask_lvls: Signal<Vec<(Decimal, u64, u64, f32)>>) {
-    // TODO: take this from ui
+    // TODO: make it a user input later
     let ticket_size = dec!(0.1);
 
     if snapshot {
       //BIDS
-      //let cuml_bids = self.add_total_volume(&bids);
       let cuml_bids = self.add_total_volume(&self.group_by_ticket_size(&bids, ticket_size));
       raw_bids.set(bids);
-      //max_total_bids.set(self.get_max_volume(&cuml_bids));
       let max_total_bids = self.get_max_volume(&cuml_bids);
       bid_lvls.set(self.add_depths(&cuml_bids, max_total_bids));
 
       //ASKS
-      // let cuml_asks = self.add_total_volume(&asks);
       let cuml_asks = self.add_total_volume(&self.group_by_ticket_size(&asks, ticket_size));
       raw_asks.set(asks);
-      // max_total_asks.set(self.get_max_volume(&cuml_asks));
       let max_total_asks = self.get_max_volume(&cuml_asks);
       ask_lvls.set(self.add_depths(&cuml_asks, max_total_asks));
     } else {
-      //TODO: may remove this in prod 
-      assert!(bids.len() == 1_000 && asks.len() == 1_000, "bids/asks length should be 100. found bids: {}, asks: {}", bids.len(), asks.len());
+
+      // assert!(bids.len() == 1_000 && asks.len() == 1_000, "bids/asks length should be 1000. found bids: {}, asks: {}", bids.len(), asks.len());
       //BIDS
       let grouped_current_bids = self.group_by_ticket_size(&bids, ticket_size);
-      //raw_bids.set(self.apply_deltas(raw_bids(), bids));
       raw_bids.set(bids);
-      //let updated_bids = self.add_total_volume(&(raw_bids)());
       let updated_bids = self.add_total_volume(&self.apply_deltas(self.group_by_ticket_size(&raw_bids(), ticket_size), grouped_current_bids, false));
-      //max_total_bids.set(self.get_max_volume(&updated_bids));
       let max_total_bids = self.get_max_volume(&updated_bids);
       bid_lvls.set(self.add_depths(&updated_bids, max_total_bids));
 
       //ASKS
       let grouped_current_asks = self.group_by_ticket_size(&asks, ticket_size);
-      // raw_asks.set(self.apply_deltas(raw_asks(), asks));
       raw_asks.set(asks);
-      //let updated_asks = self.add_total_volume(&(raw_asks)());
       let updated_asks = self.add_total_volume(&self.apply_deltas(self.group_by_ticket_size(&raw_asks(), ticket_size), grouped_current_asks, true));
-      //max_total_asks.set(self.get_max_volume(&updated_asks));
       let max_total_asks = self.get_max_volume(&updated_asks);
       ask_lvls.set(self.add_depths(&updated_asks, max_total_asks));
     }
   }
 }
-
-
